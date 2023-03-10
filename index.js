37449c6f54ed5e5c8818493e589522d02cc2dc9f
Ferales <Kuba.sadowski137@gmail.com>
2023-03-10 14:31:32 +0100

    color change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log(chessboard);
 for(let i=0; i<8; i++){
     for(let j=0; j < 8; j++){
         let square = document.createElement("div");
-        let colorClass = (i + j) % 2 == 0 ? "square-black" : "square-white";
+        let colorClass = (i + j) % 2 == 0 ? "square-white" : "square-black";
         square.classList.add(colorClass);
         chessboard.appendChild(square);
     }
