27007f409edcbfeae305b9e68802b2f81f3b33d6
Ferales <Kuba.sadowski137@gmail.com>
2023-11-07 21:18:30 +0100

    implemented rest of the pieces logic
    implemented rest of the pieces logic
    
    TODO check if piece is pinned

--- public/pieces.js
+++ public/pieces.js
@@ -29,6 +29,27 @@ export class Piece {
   getSquareId(row, column) {
     return row * 8 + column;
   }
+
+  traverseSquares(board, rowOffset, columnOffset) {
+    let legalSquares = [];
+    let [row, column] = this.getIndexes(this.squareID);
+    row += rowOffset;
+    column += columnOffset;
+
+    while (row >= 0 && row <= 7 && column >= 0 && column <= 7) {
+      if (!board[row][column]) {
+        legalSquares.push(this.getSquareId(row, column));
+      } else {
+        if (board[row][column].color != this.color) {
+          legalSquares.push(this.getSquareId(row, column));
+        }
+        break;
+      }
+      row += rowOffset;
+      column += columnOffset;
+    }
+    return legalSquares;
+  }
 }
 
 export class Pawn extends Piece {
@@ -65,12 +86,151 @@ export class Pawn extends Piece {
   }
 }
 
-export class Knight extends Piece {}
+export class Knight extends Piece {
+  constructor(color, squareID) {
+    super(color, squareID);
+  }
 
-export class Bishop extends Piece {}
+  calculateLegalMoves(board) {
+    let legalSquares = [];
+    let [row, column] = this.getIndexes();
+    let knightMoves = [
+      [-2, -1],
+      [-2, 1],
+      [-1, -2],
+      [-1, 2],
+      [1, -2],
+      [1, 2],
+      [2, -1],
+      [2, 1],
+    ];
 
-export class Rook extends Piece {}
+    for (let move of knightMoves) {
+      let newRow = row + move[0];
+      let newColumn = column + move[1];
 
-export class Queen extends Piece {}
+      if (newRow >= 0 && newRow <= 7 && newColumn >= 0 && newColumn <= 7) {
+        if (
+          (board[newRow][newColumn] &&
+            board[newRow][newColumn].color != this.color) ||
+          !board[newRow][newColumn]
+        ) {
+          legalSquares.push(this.getSquareId(newRow, newColumn));
+        }
+      }
+    }
 
-export class King extends Piece {}
+    this.legalMoves = legalSquares;
+  }
+}
+
+export class Bishop extends Piece {
+  constructor(color, squareID) {
+    super(color, squareID);
+  }
+
+  calculateLegalMoves(board) {
+    let legalSquares = [];
+    let bishopMoves = [
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ];
+
+    for (let move of bishopMoves) {
+      legalSquares = legalSquares.concat(
+        this.traverseSquares(board, move[0], move[1])
+      );
+    }
+    this.legalMoves = legalSquares;
+  }
+}
+
+export class Rook extends Piece {
+  constructor(color, squareID) {
+    super(color, squareID);
+  }
+
+  calculateLegalMoves(board) {
+    let legalSquares = [];
+    let rookMoves = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    for (let move of rookMoves) {
+      legalSquares = legalSquares.concat(
+        this.traverseSquares(board, move[0], move[1])
+      );
+    }
+    this.legalMoves = legalSquares;
+  }
+}
+
+export class Queen extends Piece {
+  constructor(color, squareID) {
+    super(color, squareID);
+  }
+
+  calculateLegalMoves(board) {
+    let legalSquares = [];
+    let queenMoves = [
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    for (let move of queenMoves) {
+      legalSquares = legalSquares.concat(
+        this.traverseSquares(board, move[0], move[1])
+      );
+    }
+    this.legalMoves = legalSquares;
+  }
+}
+
+export class King extends Piece {
+  constructor(color, squareID) {
+    super(color, squareID);
+  }
+
+  calculateLegalMoves(board) {
+    let legalSquares = [];
+    let [row, column] = this.getIndexes();
+
+    for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
+      for (let columnOffset = -1; columnOffset <= 1; columnOffset++) {
+        if (rowOffset == 0 && columnOffset == 0) {
+          continue;
+        }
+
+        let targetRow = row + rowOffset;
+        let targetColumn = column + columnOffset;
+
+        if (
+          targetRow < 0 ||
+          targetRow > 7 ||
+          targetColumn < 0 ||
+          targetColumn > 7
+        ) {
+          continue;
+        }
+
+        if (!board[targetRow][targetColumn]) {
+          legalSquares.push(this.getSquareId(targetRow, targetColumn));
+        } else if (board[targetRow][targetColumn].color != this.color) {
+          legalSquares.push(this.getSquareId(targetRow, targetColumn));
+        }
+      }
+    }
+    this.legalMoves = legalSquares;
+  }
+}
